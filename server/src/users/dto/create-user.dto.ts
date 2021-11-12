4d41f6119a869529b4422fcab67f4168d4041873
Juno <83662444+amorajuno@users.noreply.github.com>
2021-11-11 22:23:17 -0300

    Update create-user.dto.ts

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -3,7 +3,6 @@ import {
   Length,
   IsEmail,
   IsNotEmpty,
-<<<<<<< HEAD
   MinLength,
 } from 'class-validator';
 
@@ -24,27 +23,6 @@ export class CreateUserDto {
 
   @Length(6, 16, { message: 'Deve conter entre 6 e 16' })
   @IsString()
-=======
-  MaxLength,
-  MinLength,
-} from 'class-validator';
-export class CreateUserDto {
-  @IsString({ message: 'Informe um nome válido' })
-  @MinLength(2, { message: 'Informe um valor maior que 2' })
-  @IsNotEmpty()
-  name: string;
 
-  @IsNotEmpty()
-  @IsEmail({}, { message: 'Informe um endereço de email válido' })
-  @IsString()
-  email: string;
-
-  @Length(6, 16)
-  @IsString({ message: 'Informe uma senha válida' })
-  password: string;
-
-  @Length(6, 16)
-  @IsString({ message: 'Informe uma confirmação de senha válida' })
->>>>>>> 80edcedc6dc2a4d3733d43c6664597a9b9d23e9a
   passwordConfirmation: string;
 }
